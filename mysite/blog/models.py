115c4f7e64cc8c6828e9bb697bf896a704d4d2f9
ikechukwugodwin22@yahoo.com <ikechukwugodwin22@yahoo.com>
2023-09-13 12:19:51 +0100

    Create post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from 
 # Create your models here.
+
+class Post(models.Model):
+    """ Post model map to table in a db """
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+        
+    
