1941cbb815cf14f0dbdde9350b5e083d56b81b96
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2023-09-13 17:56:08 +0100

    added more attr to post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,30 @@
 from django.db import models
-from 
+from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
     """ Post model map to table in a db """
+
+    class Status(models.TextChoices):
+        """ Use to define the status or state of a blog post """
+        DRAFT = "DF", "DRAFT"
+        PUBLISHED = "PB", "PUBLISHED"
+
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    publish = models.DateTimeField()
-    
+    published = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, choices=Status.choices, default=Status.DRAFT)
+
+    # DEFINE METADATA FOR OUR MODEL
+    class Metal:
+        ordering = ["-published"]
+        indexes = [
+            models.Index(fields=["-pubblished"])
+        ]
     def __str__(self):
         return self.title
         
